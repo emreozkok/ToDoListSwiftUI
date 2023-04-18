1779adbd66da90e9b53fd19b251cd3f57006f9f7
emreozkok <emreozkok88@gmail.com>
2023-04-18 18:58:56 +0300

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Emre ÖZKÖK on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
